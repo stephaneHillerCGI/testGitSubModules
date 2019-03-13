eba3f83ac4f66b2788a98c305990890538498cd6
Hiller <stephane.hiller@cgi.com>
2019-03-13 15:15:35 +0100

    remove submodule

--- src/subModulesCommon/commonModule/src/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './my-module.module';
-export * from './pages/my-page';
-export * from './providers/my-provider';
-export * from './components/my-component';
-export * from './models/user/user.model';
